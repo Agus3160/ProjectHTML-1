b5e8c4482c2b4091aa7d48d9b3d13a006a51753d
Agus3160 <115951412+Agus3160@users.noreply.github.com>
2023-04-29 13:18:35 -0400

    Add files via upload

---
+++ js/javaScript.js
@@ -0,0 +1,14 @@
+//It allows to get a random word by the API of herokuapp (github = https://github.com/mcnaveen/Random-Words-API)
+async function fetchText() {
+    try{
+        let response = await fetch('https://random-word-api.herokuapp.com/word?lang=es');   //Get the response. 
+        return response.json().then(v => v[0]);     //Return the word that the API provide.
+    }catch(e){
+        alert("Something went wrong.\n"+ e);    //In case of error, we alert the user about it.
+    }
+}
+
+//It allow to show the random word to the user by changing the inner text of the HTML.
+document.querySelector('#main_btn').onclick = async function(){
+    document.querySelector('#random_word').innerText = await fetchText(); //Change the text of the element of id = "random_word". 
+}
